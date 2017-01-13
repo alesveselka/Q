f32d8cdff8ec06da8fb122f52f04fa4c7558e3ab
Ales Veselka <ales.veselka@gmail.com>
2017-01-13 09:55:55 +0100

    book_db: Download historical datab from Yahoo Finance

--- main/price_retrieval.py
+++ main/price_retrieval.py
@@ -27,5 +27,46 @@ def obtain_list_of_db_tickers():
         data = cursor.fetchall()
         return [(d[0], d[1]) for d in data]
 
+def get_daily_historic_data_yahoo(
+    ticker,
+    start_date=(2000,1,1),
+    end_date=datetime.date.today().timetuple()[0:3]
+    ):
+    """
+    Obtains data from Yahoo Finance, returns a list of tuples.
+
+    ticker:     Yahoo Finance ticker symbol, e. g. "GOOG" for Google, Inc.
+    start_date: Start date in (YYYY, M, D) format
+    end_date:   End date in (YYYY, M, D) format
+    """
+
+    # Construct the Yahoo URL with the correct integer query parameters
+    # for start and end dates. Note that some parameters are zero-based!
+    ticker_tuple = (
+        ticker,
+        start_date[1]-1, start_date[2], start_date[0],
+        end_date[1]-1, end_date[2], end_date[0]
+    )
+    yahoo_url = "http://ichart.finance.yahoo.com/table.csv"
+    yahoo_url += "?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s"
+    yahoo_url = yahoo_url % ticker_tuple
+
+    # Try connecting to Yahoo Finance and obtaining the data
+    # On failure, print an error message
+    try:
+        yf_data = requests.get(yahoo_url).text.split("\n")[1:-1]
+        prices = []
+        for y in yf_data:
+            p = y.strip().split(',')
+            prices.append((
+                datetime.datetime.strptime(p[0], '%Y-%m-%d'),
+                p[1], p[2], p[3], p[4], p[5], p[6]
+            ))
+    except Exeption as e:
+        print("Could not download Yahoo data: %s" % e)
+
+    return prices
+
 if __name__ == "__main__":
     # print(obtain_list_of_db_tickers())
+    print(get_daily_historic_data_yahoo("GOOG", (2000,1,1), datetime.date.today().timetuple()[0:3]))
