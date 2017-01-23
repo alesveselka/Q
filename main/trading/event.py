a6671031e51177dd11157d62b702e6ea00a807af
Ales Veselka <ales.veselka@gmail.com>
2017-01-23 22:09:42 +0100

    Create FillEvent class

--- main/trading/event.py
+++ main/trading/event.py
@@ -82,3 +82,60 @@ class OrderEvent(Event):
         """
         print 'Order: Symbol=%s, Type=%s, Quantity=%s, Direction=%s' %
               (self.symbol, self.type, self.quantity, self.direction)
+
+
+class FillEvent(Event):
+    """
+    Encapsulates the notion of a Filled Order, as returned 
+    from a brokerage. Stores the quantity of an instrument
+    actually filled and at what price. In addition, stores
+    the commission of the trade from the brokerage.
+    """
+
+    def __init__(self, timeindex, symbol, exchange, quantity,
+                 direction, fill_cost, commission=None):
+        """
+        Initialises the FillEvent object. Sets the symbol, exchange, 
+        quantity, direction, cost of fill and an optional commission.
+
+        If commission is not provided, the Fill object will 
+        calculate it based on the trade size and Interactive 
+        Brokers fees.
+
+        Parameters:
+        timeindex       The bar-resolution when the order was filled.
+        symbol          The instrument which was filled.
+        exchange        The exchange where the order was filled.
+        quantity        The filled quantity.
+        direction       The direction of fill ('BUY' or 'SELL')
+        fill_cost       The holdings value in USD.
+        commission      An optional commission sent from IB.
+        """
+        self.type = 'FILL'
+        self.timeindex = timeindex
+        self.symbol = symbol
+        self.exchange = exchange
+        self.quantity = quantity
+        self.direction = direction
+        self.fill_cost = fill_cost
+
+        if commission is None:
+            self.commission = self.calculate_ib_commission()
+        else:
+            self.commission = commission
+
+    def calculate_ib_commission(self):
+        """
+        Calculates the fees of trading based on Interactive Brokers fee
+        structure for API, in USD.
+
+        This does not include exchange or ECN fees.
+
+        Based on "US API Directed Orders"
+        """
+        full_cost = 1.3
+        if self.quantity <= 500:
+            full_cost = max(1.3, 0.013 * self.quantity)
+        else:
+            full_cost = max(1.3, 0.008 * self.quantity)
+        return full_cost
