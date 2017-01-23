366891e0677052bd2fd76d26e8e79f3c45a6c4e6
Ales Veselka <ales.veselka@gmail.com>
2017-01-23 21:29:06 +0100

    Create SignalEvent class

--- main/trading/event.py
+++ main/trading/event.py
@@ -21,3 +21,31 @@ class MarketEvent(Event):
         Initialises the MarketEvent.
         """
         self.type = 'MARKET'
+
+
+class SignalEvent(Event):
+    """
+    Handles the event of sending a Signal from a Strategy object.
+    This is received by a Portfolio object and acted upon.
+    """
+
+    def __init__(self, strategy_id, symbol, datetime, signal_type, strength):
+        """
+        Initialises the SignalEvent.
+
+        Parameters:
+        strategy_id     The unique identifier for the strategy that
+                        generated the signal.
+        symbol          The ticker symbol, e.g. 'GOOG'
+        datetime        The timestamp at which  the signal was generated.
+        signal_type     'LONG' or 'SHORT'.
+        strength        An adjustment factor 'suggestion' used to scale
+                        quantity at the portfolio level. Useful for
+                        pairs strategies.
+        """
+        self.type = 'SIGNAL'
+        self.strategy_id = strategy_id
+        self.symbol = symbol
+        self.datetime = datetime
+        self.signal_type = signal_type
+        self.strength = strength
