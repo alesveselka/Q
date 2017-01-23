1f3ad70992a61167764a3c478855be2234f974d0
Ales Veselka <ales.veselka@gmail.com>
2017-01-23 21:40:39 +0100

    Create OrderEvent class

--- main/trading/event.py
+++ main/trading/event.py
@@ -49,3 +49,36 @@ class SignalEvent(Event):
         self.datetime = datetime
         self.signal_type = signal_type
         self.strength = strength
+
+
+class OrderEvent(Event):
+    """
+    Handles the event of sending and Order to an execution system.
+    The order contains a symbol (e.g. 'GOOG'), a type (market or limit),
+    quantity and a direction.
+    """
+
+    def __init__(self, symbol, order_type, quantity, direction):
+        """
+        Initialises the OrderEvent, setting whether it is a Market 
+        order ('MKT') or Limit order ('LMT'), has a quantity (integral) 
+        and its direction ('BUY' or 'SELL').
+
+        Parameters:
+        symbol      The instrument to trade.
+        order_type  'MKT' or 'LMT' for Market or Limit.
+        quantity    Non-negative integer for quantity.
+        direction   'BUY' or 'SELL' for long or short.
+        """
+        self.type = 'ORDER'
+        self.symbol = symbol
+        self.order_type = order_type
+        self.quantity = quantity
+        self.direction = direction
+
+    def print_order(self):
+        """
+        Outputs the values within the Order.
+        """
+        print 'Order: Symbol=%s, Type=%s, Quantity=%s, Direction=%s' %
+              (self.symbol, self.type, self.quantity, self.direction)
