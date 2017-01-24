fc6c3bf72c3f2ad6e400371fec0de58e1c6d5e04
Ales Veselka <ales.veselka@gmail.com>
2017-01-24 10:27:28 +0100

    Implement inherited method in HistoricCSVDataHandler

--- main/trading/data.py
+++ main/trading/data.py
@@ -147,3 +147,82 @@ class HistoricCSVDataHandler(DataHandler):
         """
         for b in self.symbol_data[symbol]:
             yield b
+
+    def get_latest_bar(self, symbol):
+        """
+        Returns the last bar from the latest_symbol list.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print "The symbol is not available in the historical data set."
+            raise
+        else:
+            return bars_list[-1]
+
+    def get_latest_bars(self, symbol, N=1):
+        """
+        Returns the last N bars from the latest_symbol list,
+        or N-k if less is available.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print "The symbol is not available in the historical data set."
+            raise
+        else:
+            return bars_list[-N:]
+
+    def get_latest_bar_datetime(self, symbol):
+        """
+        Returns a Python datetime object for the last bar.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print "The symbol is not available in the historical data set."
+            raise
+        else:
+            return bars_list[-1][0]
+
+    def get_latest_bar_value(self, symbol, value_type):
+        """
+        Returns one of the Open, High, Low, Close, Volume, or OI
+        from the padas Bar series object.
+        """
+        try:
+            bars_list = self.latest_symbol_data[symbol]
+        except KeyError:
+            print "The symbol is not available in the historical data set."
+            raise
+        else:
+            return getattr(bars_list[-1][1], value_type)
+
+    def get_latest_bar_values(self, symbol, value_type, N=1):
+        """
+        Returns the last N bar values from the latest_symbol list,
+        or N-k if less available.
+        """
+        try:
+            bars_list = self.get_latest_bars(symbol, N)
+        except KeyError:
+            print "The symbol is not available in the historical data set."
+            raise
+        else:
+            return np.array([getattr(b[1], value_type) for b in bars_list])
+
+    def update_bars(self):
+        """
+        Pushes the latest bars to the latest_symbol_data structure
+        for all symbols in the symbol list.
+        """
+        for s in self.symbol_list:
+            try:
+                bar = next(self._get_new_bar(s))
+            except StopIteration:
+                self.continue_backtest = False
+            else:
+                if bar is not None:
+                    self.latest_symbol_data[s].append(bar)
+
+        self.events.put(MarketEvent())
