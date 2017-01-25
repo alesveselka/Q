ebe840eaa45276d8da2b323d30f91c68d278c800
Ales Veselka <ales.veselka@gmail.com>
2017-01-25 13:37:21 +0100

    Create SimulatedExecutionHandler class

--- main/trading/execution.py
+++ main/trading/execution.py
@@ -30,3 +30,39 @@ class ExecutionHandler(object):
         event   Contains an Event object with order information.
         """
         raise NotImplementedError("Should implement 'execute_order()'")
+
+
+class SimulatedExecutionHandler(ExecutionHandler):
+    """
+    The simulated execution handler simply converts all order objects
+    into their equivalent fill objects automatically without latency, 
+    slippage or fill-ratio issues.
+
+    This allows a straightforward "first go" test of any strategy,
+    before implementation with a more sophisticated execution 
+    handler.
+    """
+
+    def __init__(self, events):
+        """
+        Initializes the handler, setting the event queues up internally.
+
+        Parameters:
+        events  The Queue of Event objects.
+        """
+        self.events = events
+
+    def execute_order(self, event):
+        """
+        Simply converts Order objects into Fill objects naively,
+        i.e. without any latency, slippage or fill ratio problems.
+
+        Parameters:
+        event   Contains an Event object with order information.
+        """
+        if event.type == 'ORDER':
+            fill_event = FillEvent(
+                dt.datetime.utcnow(), event.symbol,
+                'ARCA', event.quantity, event.direction, None
+            )
+            self.events.put(fill_event)
