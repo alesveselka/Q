d71333c3c13c4a0e916b5970012b91a71ffcfe5d
Ales Veselka <ales.veselka@gmail.com>
2017-01-27 10:27:32 +0100

    Implement MA Cross strategy logic

--- main/trading/ma_cross_strategy.py
+++ main/trading/ma_cross_strategy.py
@@ -39,3 +39,50 @@ class MovingAverageCrossStrategy(Strategy):
 
         # Set to True if a symbol is in the market
         self.bought = self.__calculate_initial_bought()
+
+    def __calculate_initial_bought(self):
+        """
+        Adds keys to the bought dictionary for all symbols
+        and sets them to 'OUT'.
+        """
+        bought = {}
+        for s in self.symbol_list:
+            bought[s] = 'OUT'
+        return bought
+
+    def calculate_signals(self, event):
+        """
+        Generates a new set of signals based on the MAC SMA 
+        with the short window crossing the long window meaning
+        a long entry and vice versa for a short entry.
+
+        Parameters:
+        events  A MarketEvent object.
+        """
+        if event.type == 'MARKET':
+            for s in self.symbol_list:
+                bars = self.bars.get_latest_bar_values(
+                    s, 'adj_close', N=self.long_window
+                )
+                bar_date = self.bars.get_latest_bar_datetime(s)
+
+                if bars is not None and bars != []:
+                    short_sma = np.mean(bars[-self.short_window:])
+                    long_sma = np.mean(bars[-self.long_window:])
+
+                    symbol = s
+                    now = dt.datetime.utcnow()
+                    sig_dir = ''
+
+                    if short_sma > long_sma and self.bought == 'OUT':
+                        print 'LONG: %s' % bar_date
+                        sig_dir = 'LONG'
+                        signal = SignalEvent(1, symbol, now, sig_dir, 1.0)
+                        self.events.put(signal)
+                        self.bought[s] = 'LONG'
+                    elif short_sma < long_sma and self.bought[s] == 'LONG':
+                        print 'SHORT: %s' % bar_date
+                        sig_dir = 'EXIT'
+                        signal = SignalEvent(1, symbol, now, sig_dir, 1.0)
+                        self.events.put(signal)
+                        self.bought[s] = 'OUT'
