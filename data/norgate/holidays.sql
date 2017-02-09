7933a32c82f93fdcde05ae55e5f623e998ddfa16
alesveselka <ales.veselka@gmail.com>
2017-02-09 18:25:15 +0100

    Add 'holidays' table schema

---
+++ data/norgate/holidays.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `holidays`;
+
+CREATE TABLE `holidays`(
+  `id` int NOT NULL AUTO_INCREMENT,
+  `exchange_id` int NOT NULL,
+  `holiday` date NOT NULL,
+  `name` varchar(255) NULL,
+  `created_date` datetime NOT NULL,
+  `last_updated_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY `exchange_id_fk` (`exchange_id`) REFERENCES `exchange`(`id`) ON DELETE RESTRICT,
+  KEY `index_exchange_id` (`exchange_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
