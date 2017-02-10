e2a9f8e507f79dffebc7cf846dd30f4b068c89b3
alesveselka <ales.veselka@gmail.com>
2017-02-10 15:03:59 +0100

    Create script to automatically drop and delete MySQL schemas

---
+++ main/data_storage/norgate_db.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import MySQLdb as mysql
+
+
+def file_contents(folder, paths):
+    """
+    Load database schemas from *.sql files
+    """
+    return map(lambda path: open('%s%s.sql' % (folder, path), 'r').read(), paths)
+
+
+def commands(schemas):
+    """
+    Parse schemas passed in and returns contained commands
+    """
+    return reduce((lambda result, schema: result + schema.split('\n\n')), schemas, [])
+
+
+def execute_operation(connection, operation):
+    """
+    Connects to database and execute SQL operation passed in
+    """
+    cursor = connection.cursor()
+    cursor.execute(operation)
+
+
+if __name__ == '__main__':
+    folder = './data/norgate/'
+    schemas = [
+        'continuous_back_adjusted',
+        'continuous_spliced',
+        'contract',
+        'market',
+        'holidays',
+        'exchange',
+        'group',
+        'delivery_month'
+    ]
+    command_list = commands(file_contents(folder, schemas))
+    drop_commands = filter(lambda cmd: cmd.lower().startswith('drop'), command_list)
+    create_table_commands = filter(lambda cmd: cmd.lower().startswith('create table'), command_list)
+    connection = mysql.connect(host='localhost', user='sec_user', passwd='root', db='norgate')
+
+    print '\n'.join(drop_commands)
+    # execute_operation(connection, '\n'.join(drop_commands))
+    # execute_operation(connection, '\n'.join(reversed(create_table_commands)))
