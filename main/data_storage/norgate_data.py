8c7b4df490aa6aeadd1a58da259eb19a108d992e
alesveselka <ales.veselka@gmail.com>
2017-02-14 16:59:17 +0100

    Comment out functions

--- main/data_storage/norgate_data.py
+++ main/data_storage/norgate_data.py
@@ -6,22 +6,30 @@ import MySQLdb as mysql
 
 
 def csv_lines(file_name, exclude_header=True):
+    """
+    Read '*.csv' file by name passed in, and return non-empty rows
+    """
     reader = csv.reader(open('./data/norgate/%s.csv' % file_name), delimiter=',', quotechar='"')
     rows = [row for row in reader if re.match('^[a-zA-Z]', row[0])]
     return rows[1:] if exclude_header else rows
 
 
 def insert_values(operation, values):
+    """
+    Create MySQL database connection and cursor and execute operation for values passed in
+    """
     connection = mysql.connect(host='localhost', user='sec_user', passwd='root', db='norgate')
     with connection:
         cursor = connection.cursor()
-        cursor.executemany(operation, [v for v in values])
+        cursor.executemany(operation, values)
 
 
 def populate_exchange_table():
+    abbrev = 0
+    name = 1
     insert_values(
         'INSERT INTO exchange (abbrev, name) VALUES (%s, %s)',
-        [(l[0], l[1]) for l in csv_lines('exchange')]
+        [(l[abbrev], l[name]) for l in csv_lines('exchange')]
     )
 
 
