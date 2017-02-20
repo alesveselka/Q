5f72c995d03e072b1de73b453a0babf1a0bd6d60
alesveselka <ales.veselka@gmail.com>
2017-02-20 12:15:43 +0100

    Make 'instrument' columns not required in 'market' schema

--- data/norgate/market.sql
+++ data/norgate/market.sql
@@ -4,7 +4,7 @@ CREATE TABLE `market`(
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `code` varchar(32) NOT NULL,
-  `instrument` varchar(255) NOT NULL,
+  `instrument` varchar(255) NULL,
   `data_vendor` varchar(255) NULL,
   `exchange_id` int NOT NULL,
   `group_id` int NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE `market`(
   `oi_offset` tinyint NOT NULL DEFAULT 0,
   `delivery_months` varchar(24) NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY `exchange_id_fk` (`exchange_id`) REFERENCES `exchange`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY `group_id_fk` (`group_id`) REFERENCES `group`(`id`) ON DELETE CASCADE,
+#   FOREIGN KEY `exchange_id_fk` (`exchange_id`) REFERENCES `exchange`(`id`) ON DELETE CASCADE,
+#   FOREIGN KEY `group_id_fk` (`group_id`) REFERENCES `group`(`id`) ON DELETE CASCADE,
   KEY `index_exchange_id` (`exchange_id`),
   KEY `index_group_id` (`group_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
