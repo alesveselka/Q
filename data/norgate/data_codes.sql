ba5c124e0b00ff233808ee5062e4b0ad5e3a1357
alesveselka <ales.veselka@gmail.com>
2017-02-23 18:15:23 +0100

    Change 'data_codes' schema

--- data/norgate/data_codes.sql
+++ data/norgate/data_codes.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `data_codes`;
 
 CREATE TABLE `data_codes`(
   `code` varchar(4) NOT NULL,
-  `number` tinyint NOT NULL,
+  `appendix` varchar(4) NOT NULL,
   `name` varchar(32) NULL,
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
