3c7005c7d56e7e55a81f61a6aada91ec58f49bd5
alesveselka <ales.veselka@gmail.com>
2017-02-27 17:04:37 +0100

    Change 'currencies' schema - make 'group_id' column not null

--- data/norgate/currencies.sql
+++ data/norgate/currencies.sql
@@ -4,7 +4,7 @@ CREATE TABLE `currencies`(
   `id` int NOT NULL AUTO_INCREMENT,
   `code` varchar(8) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `group_id` int NULL,
+  `group_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `index_group_id` (`group_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
