b924bd475ea7fe7fbb01b059fe0d236e7664a4b5
alesveselka <ales.veselka@gmail.com>
2017-03-02 11:04:14 +0100

    Generate CSV file for each currency pair from downloaded data

---
+++ main/data_storage/ECU.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os
+import re
+import csv
+
+
+def read_data(dir_path, file_name):
+    reader = csv.reader(open('%s%s' % (dir_path, file_name)), delimiter=',', quotechar='"')
+    rows = [row[1:] for row in reader if re.match('^[a-zA-Z0-9]', row[0])][1:]
+    header = dict(zip(rows[0], range(len(rows[0]))))
+    data = rows[1:]
+    pairs = [r for r in rows[0] if re.match('[A-Z]{3}/[A-Z]{3}', r)]
+    date_range = file_name.split('__')[1].split('.')[0].replace('_Volume', '')
+
+    return header, data, pairs, date_range
+
+
+def generate_csvs(dir_path, header, data, pairs, date_range):
+    def generate_csv(pair):
+        values = [('{1}/{2}/{0}'.format(*d[0].split('/')), d[header[pair]]) for d in data]
+        rows = [','.join([v[0], ('0.0,' * 3)[:-1], v[1]]) for v in values if v[1]]
+
+        if len(rows):
+            file_name = '%s__%s.csv' % (pair.replace('/', ''), date_range)
+            print 'Generating %s' % file_name
+
+            f = open(''.join([dir_path, 'generated/', file_name]), 'w')
+            f.write('Date,Open,High,Low,Close\n')
+            f.write('\n'.join(rows))
+            f.close()
+
+    map(generate_csv, pairs)
+
+
+if __name__ == '__main__':
+    dir_path = './resources/UBC/'
+    dir_list = [path for path in os.listdir(dir_path) if re.match('^ECU', path)]
+
+    map(lambda file_name: generate_csvs(dir_path, *read_data(dir_path, file_name)), dir_list)
