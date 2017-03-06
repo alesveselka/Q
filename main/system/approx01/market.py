c9eea28b4905e2749d9c13dfeedda1eea80427a7
alesveselka <ales.veselka@gmail.com>
2017-03-06 21:20:31 +0100

    Add new Market and Timer classes

---
+++ main/system/approx01/market.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+class Market(object):
+
+    def __init__(self, connection, id, name, code, currency, first_data_date, group):
+        self.__connection = connection
+        self.__id = id
+        self.__name = name
+        self.__code = code
+        self.__currency = currency
+        self.__first_data_date = first_data_date
+        self.__group = group
+
+        self.__back_adjusted_data()
+
+    def __back_adjusted_data(self):
+        cursor = self.__connection.cursor()
+        sql = """
+            SELECT code, price_date, open_price, high_price, low_price, settle_price
+            FROM continuous_back_adjusted
+            WHERE market_id = '%s';
+        """
+
+        cursor.execute(sql % self.__id)
+        data = cursor.fetchall()
