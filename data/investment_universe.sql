7608d824c25b1bdc3c09979016b7e1a06ccd121a
alesveselka <ales.veselka@gmail.com>
2017-03-08 14:18:56 +0100

    Add contract and data starting dates into investment_universe schemas

--- data/investment_universe.sql
+++ data/investment_universe.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS `investment_universe`;
 CREATE TABLE `investment_universe`(
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
+  `contract_start_date` date NOT NULL,
+  `data_start_date` date NOT NULL,
   `market_ids` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
