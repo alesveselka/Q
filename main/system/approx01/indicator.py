99633e319ed0038e8cc909caf4d47990706959b3
alesveselka <ales.veselka@gmail.com>
2017-03-09 00:17:31 +0100

    Refactor EMA calculation

--- main/system/approx01/indicator.py
+++ main/system/approx01/indicator.py
@@ -30,16 +30,16 @@ def SMA(data, window):
     """
     Simple Moving Average
 
-    :param data:    List of data to compute the SMA on.
-                    Each item is tuple with two items - first is date, second actual value
+    :param data:    List of tuples(date, value) to compute the SMA on.
     :param window:  The size of 'moving window'
-    :return:        List of tuples with two items - first date, and second computed value
+    :return:        List of tuples(date, value)
     """
     w = Decimal(window)
-    nones = [(d[0], None) for d in data[:window]]
+    # nones = [(d[0], None) for d in data[:window]]
     dates, values = zip(*data)
     smas = [(dates[i], sum(values[i-window:i]) / w) for i in range(window, len(data))]
-    return nones + smas
+    # return nones + smas
+    return smas
 
 
 def EMA(data, window):
@@ -47,21 +47,31 @@ def EMA(data, window):
     Exponential Moving Average
     https://www.oanda.com/forex-trading/learn/forex-indicators/exponential-moving-average
 
-    :param data:    List of data to compute the EMA on.
-                    Each item is tuple with two items - first is date, second actual value
+    :param data:    List of tuples(date, value) to compute the EMA on.
     :param window:  The size of 'moving window'
-    :return:        List of tuples with two items - first date, and second computed value
+    :return:        List of tuples(date, value)
     """
-    ema = None
+    dates, values = zip(*data)
+    c = Decimal(2.0 / (window + 1))
+    ma = SMA(data[:window+1], window)[-1]
+    emas = []
+    for i in range(window, len(data)):
+        ma = (dates[i], (c * values[i]) + (1 - c) * ma[1])
+        emas.append(ma)
 
-    for i, d in enumerate(data):
-        start = i + 1 - window if i + 1 >= window else 0
-        settle_prices = [d[5] for d in data[start:i+1]]
-        sma = sum(settle_prices) / Decimal(len(settle_prices))
-        c = Decimal(2.0 / (len(settle_prices) + 1))
-        # TODO the above is not needed to calculate every iteration
-        ema = (c * settle_prices[-1]) + ((1 - c) * (ema or sma))
-        print d[1], start, i, len(settle_prices), settle_prices[-1], sma, ema
+    return emas
+
+    # print '*****************************************'
+    # ema = None
+    #
+    # for i, d in enumerate(data):
+    #     start = i + 1 - window if i + 1 >= window else 0
+    #     settle_prices = [d[1] for d in data[start:i+1]]
+    #     sma = sum(settle_prices) / Decimal(len(settle_prices))
+    #     c = Decimal(2.0 / (len(settle_prices) + 1))
+    #     # TODO the above is not needed to calculate every iteration
+    #     ema = (c * settle_prices[-1]) + ((1 - c) * (ema or sma))
+    #     print d[0], start, i, len(settle_prices), settle_prices[-1], sma, ema
 
 
 def ATR(data, window):
