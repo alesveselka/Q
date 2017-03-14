df9d015fdbbe8bbc850819958950234844b24fca
alesveselka <ales.veselka@gmail.com>
2017-03-14 16:14:49 +0100

    Implement Position's methods

--- main/system/approx01/position.py
+++ main/system/approx01/position.py
@@ -3,5 +3,29 @@
 
 class Position(object):
 
-    def __init__(self):
-        self._direction = 1
+    def __init__(self, signal, quantity):
+        self.__code = signal.code()
+        self.__direction = signal.direction()
+        self.__date = signal.date()
+        self.__price = signal.price()
+        self.__quantity = quantity
+
+    def code(self):
+        return self.__code
+
+    def direction(self):
+        return self.__direction
+
+    def date(self):
+        return self.__date
+
+    def price(self):
+        return self.__price
+
+    def __str__(self):
+        return ', '.join([
+            self.__code,
+            self.__direction,
+            str(self.__date),
+            str(self.__price)
+        ])[:-2]
