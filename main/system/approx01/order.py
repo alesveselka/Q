cc5b56e51d152931897a78de901d95a1ac6c1a51
alesveselka <ales.veselka@gmail.com>
2017-03-21 11:16:08 +0100

    Create Order class

---
+++ main/system/approx01/order.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+class Order(object):
+
+    def __init__(self, market, order_type, date, price, quantity):
+        self.__market = market
+        self.__type = order_type
+        self.__date = date
+        self.__price = price
+        self.__quantity = quantity
+
+    def market(self):
+        return self.__market
+
+    def type(self):
+        return self.__type
+
+    def date(self):
+        return self.__date
+
+    def price(self):
+        return self.__price
+
+    def quantity(self):
+        return self.__quantity
