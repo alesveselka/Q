9aecb2fa6ae8956e16e745a6019c24f11ab2ae12
alesveselka <ales.veselka@gmail.com>
2017-03-30 16:23:42 +0200

    Impement scraping GBP interest rate from Bank of England website

--- main/data_storage/interest_rates.py
+++ main/data_storage/interest_rates.py
@@ -15,6 +15,17 @@ import bs4
 import MySQLdb as mysql
 
 
+def page(url):
+    """
+    Fetch url and returns parsed 'soup'
+
+    :param url:     URL to fetch and parse
+    :return:        BeautifulSoup object
+    """
+    response = requests.get(url)
+    return bs4.BeautifulSoup(response.text, "html.parser")
+
+
 def aud_immediate():
     """
     Fetches and parses cash-rate page of Reserve Bank of Australia
@@ -25,13 +36,11 @@ def aud_immediate():
     | 8 Mar 2017        | 0.00                          | 1.50                          |
     +-------------------+-------------------------------+-------------------------------+
 
-    :return: List of tuples (date, float)
+    :return:        List of tuples (date, float)
     """
-    response = requests.get('http://www.rba.gov.au/statistics/cash-rate/')
-    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    soup = page('http://www.rba.gov.au/statistics/cash-rate/')
     rows = soup.select('#datatable > tbody > tr')
     rates = [[r.select('th')[0].text, r.select('td')[-1].text] for r in rows]
-    months = {k: i for i, k in enumerate(calendar.month_abbr) if k}
     result = []
 
     for r in rates:
@@ -43,22 +52,50 @@ def aud_immediate():
     return result
 
 
-def aud_three_months(connection):
+def aud_three_months():
     """
     Calculates rates from Futures data (inverted yield: 100 - interest date)
 
-    :param connection:  MySQL connector's connection instance
     :return:            List of tuples (date, float)
     """
-    cursor = connection.cursor()
+    cursor = mysql_connection.cursor()
     cursor.execute("SELECT price_date, settle_price FROM `continuous_spliced` WHERE code = 'YIR'")
     data = cursor.fetchall()
 
     return [(d[0], float(100-d[1])) for d in data]
 
 
+def gbp_immediate():
+    """
+    Fetch and parse Bank of England page with interest rates.
+    The table contains only records of interest rate changes, not regular interval!
+    +-------------------+-----------+
+    | Date Changed      | Rate      |
+    +-------------------+-----------+
+    | Thu, 04 Aug 2016  | 0.2500    |
+    +-------------------+-----------+
+
+    :return:    List of tuples (date, float)
+    """
+    soup = page('http://www.bankofengland.co.uk/boeapps/iadb/Repo.asp?Travel=NIxRPx')
+    rows = soup.select('#editorial > table > tr')
+    rates = [r.select('td') for r in rows]
+    result = []
+
+    for r in [r for r in rates if len(r)]:
+        data_date = r[0].text.split(', ')[1]
+        date = dt.date(*map(lambda x: int(x) if re.match('^[0-9]', x) else months[x], reversed(data_date.split(' '))))
+        result.append((date, float(r[1].text)))
+
+    return result
+
+
 if __name__ == '__main__':
+    months = {k: i for i, k in enumerate(calendar.month_abbr) if k}
     mysql_connection = mysql.connect('localhost', 'sec_user', 'root', 'norgate')
 
+    # TODO resolve multiple intervals (Daily, Monthly, Irregularly, ...)
+
     # aud_immediate()
-    aud_three_months(mysql_connection)
+    # aud_three_months(mysql_connection)
+    gbp_immediate()
