2209bd4be763a88bdbda6406200ae41db1fec89b
alesveselka <ales.veselka@gmail.com>
2017-04-07 12:32:55 +0200

    Move initialization code to its own operation class

--- main/system/approx01/main.py
+++ main/system/approx01/main.py
@@ -1,103 +1,10 @@
 #!/usr/bin/python
 
 import sys
-import os
-import MySQLdb as mysql
-from timer import Timer
-from risk import Risk
-from portfolio import Portfolio
-from account import Account
-from broker import Broker
-from enum import Currency
-from enum import Study
-from enum import Table
-from study import ATR, SMA, HHLL
-from investment_universe import InvestmentUniverse
-from trading_system import TradingSystem
-from data_series import DataSeries
-from decimal import Decimal
-
-
-def study_parameters():
-    short_window = 50
-    long_window = 100
-    return [
-        {'name': Study.ATR_LONG, 'study': ATR, 'window': long_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.HIGH_PRICE,
-            Table.Futures.LOW_PRICE,
-            Table.Futures.SETTLE_PRICE
-        ]},
-        {'name': Study.ATR_SHORT, 'study': ATR, 'window': short_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.HIGH_PRICE,
-            Table.Futures.LOW_PRICE,
-            Table.Futures.SETTLE_PRICE
-        ]},
-        {'name': Study.VOL_SHORT, 'study': SMA, 'window': short_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.VOLUME
-        ]},
-        {'name': Study.SMA_LONG, 'study': SMA, 'window': long_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.SETTLE_PRICE
-        ]},
-        {'name': Study.SMA_SHORT, 'study': SMA, 'window': short_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.SETTLE_PRICE
-        ]},
-        {'name': Study.HHLL_LONG, 'study': HHLL, 'window': long_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.SETTLE_PRICE
-        ]},
-        {'name': Study.HHLL_SHORT, 'study': HHLL, 'window': short_window, 'columns': [
-            Table.Futures.PRICE_DATE,
-            Table.Futures.SETTLE_PRICE
-        ]}
-    ]
-
-
-def main(universe_name):
-    timer = Timer()
-    risk_position_sizing = Decimal(0.002)
-    commission = 10.0  # TODO convert to base-currency
-    params = study_parameters()
-
-    investment_universe = InvestmentUniverse(universe_name, connection)
-    investment_universe.load_data()
-
-    data_series = DataSeries(investment_universe, connection)
-    futures = data_series.futures()
-    currency_pairs = data_series.currency_pairs()
-    interest_rates = data_series.interest_rates()
-    account = Account(Decimal(1e6), Currency.EUR, currency_pairs)
-    portfolio = Portfolio()
-    broker = Broker(timer, account, portfolio, commission, currency_pairs, interest_rates)
-    system = TradingSystem(
-        timer,
-        futures,
-        Risk(risk_position_sizing),
-        account,  # TODO access from broker?
-        portfolio,  # TODO access from broker?
-        broker
-    )
-
-    map(lambda f: f.load_data(connection) and f.calculate_studies(params), futures)
-    map(lambda cp: cp.load_data(connection), currency_pairs)
-    map(lambda r: r.load_data(connection), interest_rates)
-
-    system.subscribe()
-    broker.subscribe()
-    timer.start(investment_universe.start_data_date())
+from operations import Initialize
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and len(sys.argv[1]):
-        connection = mysql.connect(
-            os.environ['DB_HOST'],
-            os.environ['DB_USER'],
-            os.environ['DB_PASS'],
-            os.environ['DB_NAME']
-        )
-        main(sys.argv[1])
+        Initialize(sys.argv[1])
     else:
         print 'Expected one argument - name of the investment universe'
