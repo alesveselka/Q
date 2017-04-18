d62c463b5ab35a241adc165c3b8e84ec10c103fc
alesveselka <ales.veselka@gmail.com>
2017-04-18 22:40:18 +0200

    Add order sql schema

---
+++ data/order.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order`(
+  `id` int NOT NULL AUTO_INCREMENT,
+  `market_id` int NOT NULL,
+  `type` varchar(8) NOT NULL,
+  `date` date NOT NULL,
+  `price` decimal(20,10) NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
