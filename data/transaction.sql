3c4e302cf4b2db530af31aa74de0179d9cc6820d
alesveselka <ales.veselka@gmail.com>
2017-04-18 22:22:25 +0200

    Add transaction sql schema

---
+++ data/transaction.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `transaction`;
+
+CREATE TABLE `transaction`(
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(32) NOT NULL,
+  `account_action` varchar(16) NOT NULL,
+  `date` date NOT NULL,
+  `amount` decimal(20,10) NOT NULL,
+  `currency` varchar(8) NOT NULL,
+  `context` TEXT CHARACTER SET utf8,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
