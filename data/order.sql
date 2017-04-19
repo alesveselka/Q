65306f3880e650a357773c6debbc95288dd64917
alesveselka <ales.veselka@gmail.com>
2017-04-19 22:20:25 +0200

    Change order sql schema to also include result values

--- data/order.sql
+++ data/order.sql
@@ -4,8 +4,11 @@ CREATE TABLE `order`(
   `id` int NOT NULL AUTO_INCREMENT,
   `market_id` int NOT NULL,
   `type` varchar(8) NOT NULL,
+  `signal_type` varchar(16) NOT NULL,
   `date` date NOT NULL,
   `price` decimal(40,30) NOT NULL,
   `quantity` int NOT NULL,
+  `result_type` varchar(16) NOT NULL,
+  `result_price` decimal(40,30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
