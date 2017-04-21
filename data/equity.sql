d92dbfaf97982f0108ed499f3ad522b46631641a
alesveselka <ales.veselka@gmail.com>
2017-04-21 12:01:27 +0200

    Update equity sql schema with NULL columns

--- data/equity.sql
+++ data/equity.sql
@@ -6,7 +6,7 @@ CREATE TABLE `equity`(
   `date` date NOT NULL,
   `equity` decimal(40,30) NOT NULL,
   `balances` TEXT CHARACTER SET utf8 NOT NULL,
-  `margins` TEXT CHARACTER SET utf8 NOT NULL,
-  `margin_ratio` decimal(20,10) NOT NULL,
+  `margins` TEXT CHARACTER SET utf8 NULL,
+  `margin_ratio` decimal(20,10) NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
