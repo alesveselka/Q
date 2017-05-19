7a5f0d60130880e441bac7268b4c88c073e4b16d
alesveselka <ales.veselka@gmail.com>
2017-05-19 16:28:08 +0200

    Edit simulation and trding_model SQL schema to exclude unecessary trading_strategy layer

--- data/simulation.sql
+++ data/simulation.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS `simulation`;
 
 CREATE TABLE `simulation`(
-  `trading_strategy_id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `params` TEXT CHARACTER SET utf8,
+  `trading_model_id` int NOT NULL,
+  `trading_params` TEXT CHARACTER SET utf8,
+  `studies` TEXT CHARACTER SET utf8,
+  `roll_strategy_id` int NOT NULL,
   `investment_universe_id` int NOT NULL,
-  PRIMARY KEY (`trading_strategy_id`, `investment_universe_id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
--- data/trading_model.sql
+++ data/trading_model.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `trading_model`;
 
 CREATE TABLE `trading_model`(
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
