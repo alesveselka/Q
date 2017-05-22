ee061124a13de8c59721b7ce18355dadefe72180
alesveselka <ales.veselka@gmail.com>
2017-05-22 10:44:11 +0200

    Implement script inserting simulation data into DB

--- main/data_storage/simulations.py
+++ main/data_storage/simulations.py
@@ -1,80 +1,92 @@
 import os
+import json
 import MySQLdb as mysql
-
-
-def study(name, window, window_name):
-    return {'name': '%s_%s' % (name, window_name), 'study': 'ATR', 'window': window, 'columns': [
-        'price_date',
-        'high_price',
-        'low_price',
-        'settle_price'
-    ]}
-
-def simulations():
-    studies = {
-        'atr_100':
-            {'name': 'atr_long', 'study': 'ATR', 'window': 'long_window', 'columns': [
-                'price_date',
-                'high_price',
-                'low_price',
-                'settle_price'
-            ]},
-        'atr_50':
-            {'name': 'atr_short', 'study': 'ATR', 'window': 'short_window', 'columns': [
-                'price_date',
-                'high_price',
-                'low_price',
-                'settle_price'
-            ]},
-        'vol_50':
-            {'name': 'vol_short', 'study': 'SMA', 'window': 'short_window', 'columns': [
-                'price_date',
-                'volume'
-            ]},
-        'sma_100':
-            {'name': 'sma_long', 'study': 'SMA', 'window': 'long_window', 'columns': [
-                'price_date',
-                'settle_price'
-            ]},
-        'sma_50':
-            {'name': 'sma_short', 'study': 'SMA', 'window': 'short_window', 'columns': [
-                'price_date',
-                'settle_price'
-            ]},
-        'hhll_50':
-            {'name': 'hhll_short', 'study': 'HHLL', 'window': 'short_window', 'columns': [
-                'price_date',
-                'settle_price'
-            ]}
-    }
-    sim = (
-        'breakout_with_MA_filter_and_ATR_stop_1',
-        {
-            'risk_factor': 0.002,
-            'commission': 10.0,
-            'commission_currency': 'USD',
-            'interest_minimums': {'AUD': 14000, 'CAD': 14000, 'CHF': 100000, 'EUR': 100000, 'GBP': 8000, 'JPY': 11000000, 'USD': 10000},
-            'initial_balance': 1000000
-        },
-        'breakout_with_MA_filter_and_ATR_stop',
-        {
-            'short_window': 50,
-            'long_window': 100,
-            'filter_MA_type': 'SMA',
-            'volatility_MA_type': 'SMA',
-            'stop_multiple': 3,
-            'stop_window': 50
-        },
-        studies,
-        'standard_roll',
-        '25Y'
-    )
 
 
 def insert_trading_models(values):
     with mysql_connection:
         cursor = mysql_connection.cursor()
-        cursor.executemany("""INSERT INTO trading_model(name, description) VALUES (%s, %s)""", values)
+        cursor.executemany('INSERT INTO trading_model(name, description) VALUES (%s, %s)', values)
+
+
+def trading_models():
+    cursor = mysql_connection.cursor()
+    cursor.execute('SELECT id, name FROM `trading_model`')
+    return cursor.fetchall()
+
+
+def investment_universe_id(name):
+    cursor = mysql_connection.cursor()
+    cursor.execute("SELECT id FROM `investment_universe` WHERE name = '%s'" % name)
+    return cursor.fetchone()[0]
+
+
+def insert_simulations(values):
+    columns = ['name', 'params', 'trading_model_id', 'trading_params', 'studies', 'roll_strategy_id', 'investment_universe_id']
+    command = 'INSERT INTO `simulation` (%s) VALUES(%s)' % (', '.join(columns), ('%s, ' * len(columns))[:-2])
+
+    with mysql_connection:
+        cursor = mysql_connection.cursor()
+        cursor.executemany(command, values)
+
+
+def studies(data):
+    return [{'name': '%s_%s' % (d[0], d[1]), 'study': d[2], 'window': d[3], 'columns': d[4]} for d in data]
+
+
+def simulation_params(initial_balance, risk_factor):
+    return {
+        'initial_balance': initial_balance,
+        'risk_factor': risk_factor,
+        'commission': 10.0,
+        'commission_currency': 'USD',
+        'interest_minimums': {
+            'AUD': 14000,
+            'CAD': 14000,
+            'CHF': 100000,
+            'EUR': 100000,
+            'GBP': 8000,
+            'JPY': 11000000,
+            'USD': 10000
+        },
+        'slippage_map': [
+            {'atr': 2, 'min': 0, 'max': 100},
+            {'atr': 1, 'min': 100, 'max': 1000},
+            {'atr': 0.25, 'min': 1000, 'max': 10000},
+            {'atr': 0.1, 'min': 10000, 'max': 50000},
+            {'atr': 0.05, 'min': 50000, 'max': 200000},
+            {'atr': 0.01, 'min': 200000, 'max': 1e9}
+        ]
+    }
+
+
+def simulations(investment_universe_name):
+    trading_model_id, trading_model_name = trading_models()[0]
+    trading_params = {
+        'short_window': 50,
+        'long_window': 100,
+        'filter_MA_type': 'SMA',
+        'volatility_MA_type': 'SMA',
+        'stop_multiple': 3,
+        'stop_window': 50
+    }
+    study_map = studies([
+        ('atr', 'long', 'ATR', 100, ['price_date', 'high_price', 'low_price', 'settle_price']),
+        ('atr', 'short', 'ATR', 50, ['price_date', 'high_price', 'low_price', 'settle_price']),
+        ('ma', 'long', 'SMA', 100, ['price_date', 'settle_price']),
+        ('ma', 'short', 'SMA', 50, ['price_date', 'settle_price']),
+        ('vol', 'short', 'SMA', 50, ['price_date', 'volume']),
+        ('hhll', 'short', 'HHLL', 50, ['price_date', 'settle_price'])
+    ])
+    return [(
+        '%s_1' % trading_model_name,
+        json.dumps(simulation_params(1e6, 0.002)),
+        trading_model_id,
+        json.dumps(trading_params),
+        json.dumps(study_map),
+        0,
+        investment_universe_id(investment_universe_name)
+    )]
 
 
 if __name__ == '__main__':
@@ -84,8 +96,9 @@ if __name__ == '__main__':
         os.environ['DB_PASS'],
         os.environ['DB_NAME']
     )
-    trading_models = [(
+    trading_model_data = [(
         'breakout_with_MA_filter_and_ATR_stop',
         'Highest-high and Lowest-low breakout with Moving Average trend filter and ATR volatility based exit stops'
     )]
-    insert_trading_models(trading_models)
+    # insert_trading_models(trading_model_data)
+    insert_simulations(simulations('25Y'))
