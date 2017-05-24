727ee84f1213956df1b62de9289c261c39fa08e7
alesveselka <ales.veselka@gmail.com>
2017-05-24 10:29:43 +0200

    Remove unused roll strategy SQL schema

--- data/roll_strategy.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `roll_strategy`;
-
-CREATE TABLE `roll_strategy`(
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
