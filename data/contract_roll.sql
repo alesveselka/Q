df133b21115a30848c5813456bc886dd10375f1c
alesveselka <ales.veselka@gmail.com>
2017-06-01 10:02:40 +0200

    Add contract_roll SQL schema

---
+++ data/contract_roll.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `contract_roll`;
+
+CREATE TABLE `contract_roll`(
+  `id` int NOT NULL AUTO_INCREMENT,
+  `market_id` int NOT NULL,
+  `roll_strategy_id` int NOT NULL,
+  `date` date NOT NULL,
+  `gap` decimal(20,10) NULL,
+  `roll_out_contract` VARCHAR(32) NOT NULL,
+  `roll_in_contract` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
