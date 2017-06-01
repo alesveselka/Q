2233492b3e7bbedd4bb2fc1ca4210a52d04950a0
alesveselka <ales.veselka@gmail.com>
2017-06-01 19:26:00 +0200

    Find 'roll in' info by roll out date and roll-schedule

---
+++ main/processing/contracts.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+
+import os
+import csv
+import re
+import json
+import calendar
+import datetime as dt
+import MySQLdb as mysql
+from operator import itemgetter
+
+
+def csv_lines(path, exclude_header=True, filter_index=0):
+    reader = csv.reader(open(path), delimiter=',', quotechar='"')
+    rows = [row for row in reader if re.match('^[a-zA-Z0-9]', row[filter_index])]
+    return rows[1:] if exclude_header else rows
+
+
+def query(table, columns, placeholders):
+    return 'INSERT INTO `%s` (%s) VALUES (%s)' % (table, columns, placeholders)
+
+
+def insert_values(operation, values):
+    with mysql_connection:
+        cursor = mysql_connection.cursor()
+        cursor.executemany(operation, values)
+
+
+def construct_continuous():
+    roll_strategy_name = 'standard_roll_1'
+    cursor = mysql_connection.cursor()
+    cursor.execute("SELECT code, appendix FROM `data_codes`")
+    data_codes = dict(cursor.fetchall())
+    cursor.execute("SELECT id, code, data_codes, volume_offset, oi_offset FROM `market`")
+    codes = cursor.fetchall()
+    cursor.execute("SELECT code, name, short_name FROM `delivery_month`")
+    delivery_months = cursor.fetchall()
+    cursor.execute("SELECT id, name, params FROM `roll_strategy` WHERE name = '%s'" % roll_strategy_name)
+    roll_strategy = cursor.fetchone()
+    roll_strategy_id = roll_strategy[0]
+    roll_strategy_params = json.loads(roll_strategy[2])
+    cursor.execute("""
+        SELECT 
+            market_id, 
+            roll_out_month, 
+            roll_in_month, 
+            month, 
+            day 
+        FROM `standard_roll_schedule` 
+        WHERE name = '%s'""" % roll_strategy_params['schedule'])
+    roll_schedule = cursor.fetchall()
+    dir_path = './resources/Norgate/data/Futures/Contracts/_Text/'
+    dir_list = os.listdir(dir_path)
+    now = dt.datetime.now()
+    all_codes = reduce(lambda result, c: result + map(lambda d: (c[0], c[1] + data_codes[d], c[3], c[4]), c[2]), codes, [])
+    matching_codes = filter(lambda c: c[1] in dir_list, all_codes)
+    columns = [
+        'market_id',
+        'roll_strategy_id',
+        'delivery_date',
+        'expiration_date',
+        'code',
+        'price_date',
+        'open_price',
+        'high_price',
+        'low_price',
+        'last_price',
+        'settle_price',
+        'volume',
+        'open_interest',
+        'created_date',
+        'last_updated_date'
+    ]
+    q = query('continuous_adjusted', ','.join(columns), ("%s, " * len(columns))[:-2])
+    market_id = 0
+    roll_out_month = 1
+    roll_in_month = 2
+    month = 3
+    day = 4
+    month_abbrs = [m for m in calendar.month_abbr]
+
+    # print sorted(matching_codes, key=itemgetter(1))
+
+    populate_symbol(
+        now,
+        (37L, 'ES', 0, -1),
+        # (106L, 'WT', 0, -1),
+        roll_strategy_id,
+        [(r[roll_out_month], r[roll_in_month], r[month], r[day]) for r in roll_schedule if r[market_id] == 37],
+        dir_path,
+        delivery_months,
+        q,
+        month_abbrs
+    )
+
+    # for code in matching_codes:
+    #     populate_symbol(now, code, dir_path, delivery_months, q)
+
+
+def populate_symbol(now, code, roll_strategy_id, roll_schedule, dir_path, delivery_months, q, month_abbrs):
+    files = os.listdir(''.join([dir_path, code[1]]))
+
+    def index(key, data, position=0):
+        return reduce(lambda i, d: i + 1 if d[position] <= key else i, data, 0)
+
+    def values(file_name):
+        delivery = file_name[5:10]
+        year = int(delivery[:-1])
+        month = index(delivery[-1], delivery_months)
+        delivery_date = dt.date(year, month, calendar.monthrange(year, month)[1])
+        rows = csv_lines(''.join([dir_path, code[1], '/', file_name]), exclude_header=False)
+        return rows
+        # price_date = 0
+        # open_price = 1
+        # high_price = 2
+        # low_price = 3
+        # last_price = 4
+        # settle_price = 4
+        # volume = 5
+        # open_interest = 6
+        # return [[
+        #     code[0],
+        #     roll_strategy_id,
+        #     delivery_date,
+        #     delivery_date,
+        #     code[1] + delivery,
+        #     r[price_date],
+        #     r[open_price],
+        #     r[high_price],
+        #     r[low_price],
+        #     r[last_price],
+        #     r[settle_price],
+        #     r[volume],
+        #     r[open_interest],
+        #     now,
+        #     now,
+        #     month_abbrs[month]
+        # ] for r in rows]
+
+    # map(lambda f: insert_values(q, values(f)), files)
+
+    # filter the file names by the delivery months in roll schedule
+    price_date = 0
+    open_price = 1
+    high_price = 2
+    low_price = 3
+    last_price = 4
+    settle_price = 4
+    volume = 5
+    open_interest = 6
+    continuous = []
+    schedule_months = [s[0] for s in roll_schedule]
+    file_names = [f for f in files if month_abbrs[index(f[-5], delivery_months, 0)] in schedule_months]
+    for f in file_names:
+        roll = roll_in_info(f, roll_schedule, delivery_months, month_abbrs)
+        roll_in_file = roll[0]
+        if roll_in_file in file_names:
+            roll_in_rows = csv_lines(''.join([dir_path, code[1], '/', roll_in_file]), exclude_header=False)
+            # continuous += [r for r in roll_in_rows if date(r[price_date]) < ]
+
+
+        print f, roll
+
+    print roll_strategy_id
+
+    for s in roll_schedule:
+        print s
+
+
+def roll_in_info(roll_out_file, roll_schedule, delivery_months, month_abbrs):
+    out_year = int(roll_out_file[5:9])
+    out_month_code = roll_out_file[-5]
+    out_month_index = index(out_month_code, delivery_months, 0) + 1
+    out_month = month_abbrs[out_month_index]
+    roll = [s for s in roll_schedule if s[0] == out_month][0]
+    in_month = roll[1]
+    in_month_code = [m for m in delivery_months if m[2] == in_month][0][0]
+    in_month_index = index(in_month_code, delivery_months, 0) + 1
+    in_year = out_year if in_month_index > out_month_index else int(out_year) + 1
+    roll_day = roll[3]
+    roll_month = roll[2]
+    roll_month_index = index(roll_month, delivery_months, 2) + 1
+    roll_year = out_year if out_month_index - roll_month_index > -1 else out_year - 1
+    in_file = roll_out_file.replace(str(out_year), str(in_year)).replace(out_month_code, in_month_code)
+    return in_file, roll_year, roll_month, roll_day
+
+
+def index(item, sequence, item_filter):
+    return [i for i, s in enumerate(sequence) if s[item_filter] == item][0]
+
+
+def date(d):
+    return dt.date(int(d[:4]), int(d[4:6]), int(d[:6]))
+
+
+def populate_continuous_adjusted(schema):
+    populate_continuous(
+        schema,
+        './resources/Norgate/data/Futures/Contracts/_Text/'
+    )
+
+
+def populate_continuous(schema, dir_path):
+    cursor = mysql_connection.cursor()
+    cursor.execute("SELECT code, appendix FROM `data_codes`")
+    data_codes = dict(cursor.fetchall())
+    cursor.execute("SELECT id, code, data_codes FROM `market`")
+    codes = cursor.fetchall()
+    cursor.execute("SELECT id, name FROM `roll_strategy` WHERE name = 'norgate'")
+    roll_strategy_id = cursor.fetchone()[0]
+    dir_list = [d.split('.')[0] for d in os.listdir(dir_path)]
+    now = dt.datetime.now()
+    all_codes = reduce(lambda result, c: result + map(lambda d: (c[0], c[1] + data_codes[d]), c[2]), codes, [])
+    matching_codes = filter(lambda c: c[1] in dir_list, all_codes)
+    columns = [
+        'market_id',
+        'roll_strategy_id',
+        'code',
+        'price_date',
+        'open_price',
+        'high_price',
+        'low_price',
+        'last_price',
+        'settle_price',
+        'volume',
+        'open_interest',
+        'created_date',
+        'last_updated_date'
+    ]
+    q = query(schema, ','.join(columns), ("%s, " * len(columns))[:-2])
+
+    def values(code):
+        rows = csv_lines(''.join([dir_path, code[1], '.csv']), exclude_header=False)
+        return [[code[0], roll_strategy_id, code[1], r[0], r[1], r[2], r[3], r[4], r[4], r[5], r[6], now, now] for r in rows]
+
+    map(lambda c: insert_values(q, values(c)), matching_codes)
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) == 2 and len(sys.argv[1]):
+    #     schema = sys.argv[1]
+
+    mysql_connection = mysql.connect(
+        os.environ['DB_HOST'],
+        os.environ['DB_USER'],
+        os.environ['DB_PASS'],
+        os.environ['DB_NAME']
+    )
+
+    construct_continuous()
