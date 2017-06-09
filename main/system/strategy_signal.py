6de439284882d4ff235dd96416def5476a09b6da
alesveselka <ales.veselka@gmail.com>
2017-06-09 13:50:44 +0200

    Add contract code to the signal, order and position fields

--- main/system/strategy_signal.py
+++ main/system/strategy_signal.py
@@ -3,12 +3,13 @@
 
 class Signal(object):
 
-    def __init__(self, market, type, direction, date, price):
+    def __init__(self, market, type, direction, date, price, contract):
         self.__market = market
         self.__type = type
         self.__direction = direction
         self.__date = date
         self.__price = price
+        self.__contract = contract
 
     def market(self):
         return self.__market
@@ -25,11 +26,15 @@ class Signal(object):
     def price(self):
         return self.__price
 
+    def contract(self):
+        return self.__contract
+
     def __str__(self):
         return ', '.join([
             self.__market.code(),
             self.__type,
             self.__direction,
             str(self.__date),
-            str(self.__price)
+            str(self.__price),
+            str(self.__contract)
         ])
