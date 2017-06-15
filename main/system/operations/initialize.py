158428106ed6fc4e1954b15caed998baf71af371
alesveselka <ales.veselka@gmail.com>
2017-06-15 10:44:52 +0200

    Pass commission currency to the data series to be included in loaded data

--- main/system/operations/initialize.py
+++ main/system/operations/initialize.py
@@ -30,17 +30,18 @@ class Initialize:
 
         precision = getcontext().prec
         risk_position_sizing = Decimal('%s' % params['risk_factor']).quantize(Decimal('1.' + ('0' * precision)))
-        commission = (params['commission'], params['commission_currency'])
-        interest_minimums = params['interest_minimums']
         base_currency = params['base_currency']
+        commission_currency = params['commission_currency']
+        commission = (params['commission'], commission_currency)
+        interest_minimums = params['interest_minimums']
 
         investment_universe = InvestmentUniverse(simulation[Table.Simulation.INVESTMENT_UNIVERSE], connection)
         investment_universe.load_data()
 
         data_series = DataSeries(investment_universe, connection, simulation[Table.Simulation.STUDIES])
         futures = data_series.futures(roll_strategy[Table.RollStrategy.ID], params['slippage_map'])
-        currency_pairs = data_series.currency_pairs(base_currency)
-        interest_rates = data_series.interest_rates()
+        currency_pairs = data_series.currency_pairs(base_currency, commission_currency)
+        interest_rates = data_series.interest_rates(base_currency, commission_currency)
 
         account = Account(Decimal(params['initial_balance']), base_currency, currency_pairs)
         broker = Broker(account, commission, currency_pairs, interest_rates, interest_minimums)
