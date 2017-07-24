f66a84af63c5d21c87497257c091d4e12f445a7a
alesveselka <ales.veselka@gmail.com>
2017-07-24 10:49:13 +0200

    Calculate markets' volatilities

--- main/data_storage/correlations.py
+++ main/data_storage/correlations.py
@@ -3,7 +3,9 @@
 import os
 import datetime as dt
 import MySQLdb as mysql
+from math import log
 from math import sqrt
+from itertools import combinations
 
 
 connection = mysql.connect(
@@ -30,19 +32,14 @@ def __investment_universe(name):
     return cursor.fetchone()
 
 
-def __markets(market_ids):
+def __market_codes(market_id):
     cursor = connection.cursor()
-    market_query = """
+    cursor.execute("""
         SELECT name, code, data_codes
         FROM market
         WHERE id = '%s';
-    """
-    market_data = []
-    for market_id in market_ids:
-        cursor.execute(market_query % market_id)
-        market_data.append(cursor.fetchone())
-
-    return market_data
+    """ % market_id)
+    return cursor.fetchone()
 
 
 def market_series(market_id, market_code, roll_strategy_id, start_date, end_date):
@@ -73,21 +70,73 @@ def __std(values):
     return sqrt(sum((v - mean)**2 for v in values) / (length - 1))
 
 
+def __volatility_series(market_id, roll_strategy_id, start_date, end_date, lookback):
+    codes = __market_codes(market_id)
+    code = ''.join([codes[1], '2']) if 'C' in codes[2] else codes[1]
+    price_series = market_series(market_id, code, roll_strategy_id, start_date, end_date)
+
+    log_returns = []
+    returns_squared = []
+    result = [price_series[0]]
+    for i, item in enumerate(price_series[1:]):
+        price = item[1]
+        log_returns.append(log(price / price_series[i][1]))
+        returns_squared.append(log_returns[-1] ** 2)
+        std = __std(log_returns[-lookback:]) if i else 0.0
+        vol = sqrt(sum(returns_squared[-lookback:]) / lookback) if i >= lookback - 1 else 0.0
+
+        result.append((item[0], price, log_returns[-1], returns_squared[-1], vol, std))
+
+        print '%s, %.2f, %.8f, %.8f, %.8f, %.8f' % (
+            result[-1][0],
+            result[-1][1],
+            result[-1][2],
+            result[-1][3],
+            result[-1][4],
+            result[-1][5]
+        )
+
+
+
+# def __correlations(market_id1, market_id2):
+
+
 def main():
-    roll_strategy = __roll_strategy('standard_roll_1')
+    # roll_strategy = __roll_strategy('standard_roll_1')
+    roll_strategy = __roll_strategy('norgate')
     investment_universe = __investment_universe('25Y')
     start_contract_date = investment_universe[0]
     start_data_date = investment_universe[1]
-    end_date = dt.date(2015, 12, 31)
+    start_date = dt.date(1900, 1, 1)
+    end_date = dt.date(9999, 12, 31)
     market_ids = investment_universe[2].split(',')
-    markets = __markets(market_ids)
-    series = market_series(33, 'W2', roll_strategy[0], start_contract_date, end_date)
+    # markets = __market_codes(market_ids)
+    series_w = market_series(33, 'W2', roll_strategy[0], start_date, end_date)
+    series_kw = market_series(25, 'KW2', roll_strategy[0], start_date, end_date)
 
-    # print len(series)
-    # for s in series[-10:]:
+    print 'start_contract_date', investment_universe
+
+    # print len(series_w)
+    # for s in series_w[:10]:
     #     print s
 
-    print 'std: ', __std([-0.00031358, 0.00265584])
+    # print len(series_kw)
+    # for s in series_kw[:10]:
+    #     print s
+
+    # print 'std: ', __std([-0.00031358, 0.00265584])
+
+    market_id_pairs = [c for c in combinations(map(int, market_ids), 2)]
+
+    # for pair in market_id_pairs:
+    #     print pair
+
+    # print market_ids
+    print len(market_ids)
+    print len(market_id_pairs)
+
+    # __volatility_series(33, roll_strategy[0], start_date, end_date, 25)
+    __volatility_series(55, roll_strategy[0], dt.date(2007, 1, 3), dt.date(2008, 12, 31), 25)
 
 
 if __name__ == '__main__':
