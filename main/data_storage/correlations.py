1cb0ab2f1749094e8b5f3acef21838eeb5f8cff8
alesveselka <ales.veselka@gmail.com>
2017-07-25 15:50:29 +0200

    Calculate movement correlation

--- main/data_storage/correlations.py
+++ main/data_storage/correlations.py
@@ -77,26 +77,36 @@ def __volatility_series(price_series, lookback):
     
     :param price_series:    price series to calculate volatility on
     :param lookback:        lookback number for the vol. calculation
-    :return:                list of tuples(date, price, return, stdev, volatility)
+    :return:                tuple of list of tuples(
+                                date
+                                price, 
+                                return, 
+                                deviation, 
+                                deviation-squared, 
+                                deviation-volatility, 
+                                movement-volatility
+                            ), and dict of indexes
     """
     smooth_factor = 5
     returns = []
-    devs = []
-    devs_squared = []
-    result = [(price_series[0][0], price_series[0][1], 0.0, 0.0, 0.0, 0.0, 0.0)]
+    deviations = []
+    deviations_squared = []
+    result = [(price_series[0][0], price_series[0][1], None, None, None, None, None)]
+    indexes = {price_series[0][0]: 0}
     for i, item in enumerate(price_series[smooth_factor:]):
         price = item[1]
         prev_price = price_series[i][1]
         ret = abs(price / prev_price - 1)
         returns.append(ret * -1 if price < prev_price else ret)
         return_window = returns[-lookback:]
-        devs.append(return_window[-1] - sum(return_window) / len(return_window))
-        devs_squared.append(devs[-1]**2)
-        dev_vol = sqrt(sum(devs_squared[-lookback:]) / (lookback - 1)) if i >= lookback - 1 else 0.0
-        movement_vol = sqrt(sum(r**2 for r in return_window) / lookback) if i >= lookback - 1 else 0.0
-        result.append((item[0], price, returns[-1], devs[-1], devs_squared[-1], dev_vol, movement_vol))
+        deviations.append(return_window[-1] - sum(return_window) / len(return_window))
+        deviations_squared.append(deviations[-1]**2)
+        deviation_vol = sqrt(sum(deviations_squared[-lookback:]) / (lookback - 1)) if i >= lookback - 1 else None
+        movement_vol = sqrt(sum(r**2 for r in return_window) / lookback) if i >= lookback - 1 else None
+        result.append((item[0], price, returns[-1], deviations[-1], deviations_squared[-1], deviation_vol, movement_vol))
+        indexes[item[0]] = len(result) - 1
 
-    return result
+    return result, indexes
 
 
 # def __correlations(market_id1, market_id2):
@@ -115,23 +125,46 @@ def main():
     # SP = 55
     # TU = 79
 
+    start_date = dt.date(2007, 1, 3)
+    end_date = dt.date(2008, 12, 31)
     lookback = 25
     volas = {}
     # for market_id in market_ids:
-    for market_id in [55]:
-        print 'calculating', market_id
-        price_series = market_series(market_id, dt.date(2007, 1, 3), dt.date(2007, 12, 31))
-        # price_series = market_series(market_id, start_date, end_date)
+    for market_id in [55, 79]:
+        print 'calculating volatility', market_id
+        price_series = market_series(market_id, start_date, end_date)
         volas[market_id] = __volatility_series(price_series, lookback)
 
-        # for p in price_series[5:30]:
-        #     print p
-
-    # for v in volas[27]:
-    #     print v
+    date_range = [start_date + dt.timedelta(days=i) for i in xrange(0, (end_date - start_date).days + 1)]
+    DATE = 0
+    PRICE = 1
+    RETURN = 2
+    DEVIATION = 3
+    DEVIATION_SQUARED = 4
+    DEVIATION_VOL = 5
+    MOVEMENT_VOL = 6
 
     # for pair in market_id_pairs:
-    # for pair in [(55, 79)]:
+    for pair in [(55, 79)]:
+        print 'calculating correlation', pair
+        vol_a, vol_a_indexes = volas[pair[0]]
+        vol_b, vol_b_indexes = volas[pair[1]]
+
+        for i, date in enumerate(date_range):
+            if date in vol_a_indexes and vol_a[vol_a_indexes[date]][DEVIATION_VOL] and date in vol_b_indexes and vol_b[vol_b_indexes[date]][DEVIATION_VOL]:
+            # vol_a_record = [v for v in vol_a if v[DATE] == date]
+            # vol_b_record = [v for v in vol_a if v[DATE] == date]
+            # if date in vol_a and vol_a[date][DEVIATION_VOL] and date in vol_b and vol_b[date][DEVIATION_VOL]:
+            # if len(vol_a_record) and vol_a_record[0][DEVIATION_VOL] and len(vol_b_record) and vol_b_record[0][DEVIATION_VOL]:
+
+                index_a = vol_a_indexes[date] + 1
+                index_b = vol_b_indexes[date] + 1
+                returns_a = [v[RETURN] for v in vol_a[index_a-lookback:index_a]]
+                returns_b = [v[RETURN] for v in vol_b[index_b-lookback:index_b]]
+
+                movement_correlation = sum(r[0] * r[1] for r in zip(returns_a, returns_b)) / (lookback * vol_a[vol_a_indexes[date]][MOVEMENT_VOL] * vol_b[vol_b_indexes[date]][MOVEMENT_VOL])
+
+                print i, date, movement_correlation
 
 
 if __name__ == '__main__':
