9bfdb9c6b71dc58d3a4cf5e803520e750bdae437
alesveselka <ales.veselka@gmail.com>
2017-08-03 13:49:21 +0200

    Add 'investment_universe_name' to the 'group_correlation' SQL schema

--- data/group_correlation.sql
+++ data/group_correlation.sql
@@ -4,6 +4,7 @@ CREATE TABLE `group_correlation`(
   `id` int NOT NULL AUTO_INCREMENT,
   `group_id` int NOT NULL,
   `lookback` int NOT NULL,
+  `investment_universe_name` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `returns` double NULL,
   `movement_volatility` double NULL,
@@ -12,5 +13,6 @@ CREATE TABLE `group_correlation`(
   `dev_correlations` TEXT CHARACTER SET utf8 NULL,
   PRIMARY KEY (`id`),
   KEY `index_lookback` (`lookback`),
-  KEY `index_group_id` (`group_id`, `lookback`)
+  KEY `index_lookback_universe` (`lookback`, `investment_universe_name`),
+  KEY `index_group_id` (`group_id`, `lookback`, `investment_universe_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
