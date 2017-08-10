4f55113d40b09f4f65357c8f04173923e283016b
alesveselka <ales.veselka@gmail.com>
2017-08-10 10:33:59 +0200

    Add columns for exponentially weghted correlation values

--- data/market_correlation.sql
+++ data/market_correlation.sql
@@ -9,7 +9,9 @@ CREATE TABLE `market_correlation`(
   `movement_volatility` double NULL,
   `dev_volatility` double NULL,
   `movement_correlations` TEXT CHARACTER SET utf8 NULL,
+  `movement_correlations_ew` TEXT CHARACTER SET utf8 NULL,
   `dev_correlations` TEXT CHARACTER SET utf8 NULL,
+  `dev_correlations_ew` TEXT CHARACTER SET utf8 NULL,
   PRIMARY KEY (`id`),
   KEY `index_lookback` (`lookback`),
   KEY `index_market_id` (`market_id`, `market_code`, `lookback`)
