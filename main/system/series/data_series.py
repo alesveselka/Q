f63a92b802276bc87dd822b095c8ee731868c837
alesveselka <ales.veselka@gmail.com>
2017-08-16 12:38:02 +0200

    Cache loaded roll strategy to pass into market objects

--- main/system/series/data_series.py
+++ main/system/series/data_series.py
@@ -54,6 +54,13 @@ class DataSeries:
             self.__futures = []
 
             series_class = NorgateSeries if roll_strategy[Table.RollStrategy.NAME] == 'norgate' else CustomSeries
+            loaded_roll_strategy = (
+                roll_strategy[Table.RollStrategy.ID],
+                roll_strategy[Table.RollStrategy.NAME],
+                roll_strategy[Table.RollStrategy.TYPE],
+                json.loads(roll_strategy[Table.RollStrategy.PARAMS])
+                if roll_strategy[Table.RollStrategy.PARAMS] else None,
+            )
 
             for market_id in self.__investment_universe.market_ids():
             # for market_id in [33]:  # 100 = CL2, 33 = W2, 19 = SB
@@ -61,12 +68,7 @@ class DataSeries:
                 self.__futures.append(Market(
                     market_id,
                     slippage_map,
-                    series_class(start_data_date, self.__study_parameters, (
-                        roll_strategy[Table.RollStrategy.ID],
-                        roll_strategy[Table.RollStrategy.NAME],
-                        roll_strategy[Table.RollStrategy.TYPE],
-                        json.loads(roll_strategy[Table.RollStrategy.PARAMS]),
-                    )),
+                    series_class(start_data_date, self.__study_parameters, loaded_roll_strategy),
                     *cursor.fetchone())
                 )
 
