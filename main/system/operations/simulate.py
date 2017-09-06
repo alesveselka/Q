437030a205e69d3608c2c30f8e3010f0ec4b4737
alesveselka <ales.veselka@gmail.com>
2017-09-06 23:09:04 +0200

    Calculate correlation weights in Portfolio object, first draft

--- main/system/operations/simulate.py
+++ main/system/operations/simulate.py
@@ -13,8 +13,6 @@ from order import Order
 from timer import Timer
 from order_result import OrderResult
 from persist import Persist
-from itertools import combinations
-from collections import defaultdict
 
 
 class Simulate:
@@ -35,7 +33,7 @@ class Simulate:
         now = dt.datetime.now()
         # end_date = dt.date(now.year, now.month, now.day)
         # end_date = dt.date(1992, 6, 10)
-        end_date = dt.date(1992, 12, 31)
+        end_date = dt.date(1992, 5, 31)
 
         self.__data_series.load(end_date, roll_strategy[Table.RollStrategy.ID])
         self.__subscribe()
@@ -111,21 +109,10 @@ class Simulate:
         # and also for simpler and faster calculations
         self.__data_series.update_futures_data(date)
 
-        self.__transfer_orders(self.__orders(date))
+        # TODO before transferring calculate weights and construct DIFF
+        self.__portfolio.candidate(date)
 
-        markets = [p.market() for p in self.__portfolio.open_positions()]
-        if len(markets) >= 2:
-            market_ids = {str(m.id()): m for m in markets}
-            pairs = list(combinations(market_ids.keys(), 2))
-            counter = defaultdict(int)
-            fraction = .25
-            print date, market_ids.keys(), pairs
-            for pair in pairs:
-                correlation = market_ids[pair[0]].correlation(date, pair[1])
-                rounded_correlation = abs(fraction * round(correlation/fraction)) if correlation else None
-                counter[rounded_correlation] += 1
-                print pair, correlation, rounded_correlation
-            print counter
+        self.__transfer_orders(self.__orders(date))
 
     def __on_market_close(self, date, previous_date):
         """
@@ -203,6 +190,7 @@ class Simulate:
                     orders.append(Order(market, signal, date, open_price, market_position.quantity(), contract))
 
                 if market_position is None and signal in enter_signals:
+                    # TODO don't calculate Qty -- it will be calculated with relations to open positions as well.
                     quantity = self.__risk.position_size(market.point_value(), market.currency(), atr_long, date)
                     orders.append(Order(market, signal, date, open_price, quantity, market.contract(date)))
 
--- main/system/portfolio.py
+++ main/system/portfolio.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+from math import log
+from operator import mul
+from itertools import combinations
+from collections import defaultdict
+
 
 class Portfolio(object):
 
@@ -23,3 +28,48 @@ class Portfolio(object):
 
     def closed_positions(self):
         return self.__closed_positions
+
+    def candidate(self, date):
+        self.__correlation_weights(date)
+        self.__volatility_scalars(date)
+        self.__diversification_multiplier(date)
+
+    def __correlation_weights(self, date):
+        # TODO The 'Handcrafting' method assumes the assets have the same expected standard deviation of returns!
+        # TODO also incorporate new orders and create DIFF
+        markets = [p.market() for p in self.__positions]
+        if len(markets) >= 2:
+            market_ids = {str(m.id()): m for m in markets}
+            pairs = list(combinations(market_ids.keys(), 2))
+            groups = defaultdict(list)
+            market_correlations = defaultdict(list)
+            group_correlations = defaultdict(list)
+            fraction = .25
+            print date, market_ids.keys(), pairs
+            for pair in pairs:
+                correlation = abs(market_ids[pair[0]].correlation(date, pair[1]))
+                correlation = 1e-6 if correlation == 0.0 else correlation
+                market_correlations[pair[0]].append(correlation)
+                market_correlations[pair[1]].append(correlation)
+                rounded_correlation = (fraction * round(correlation/fraction)) if correlation else None
+                group_correlations[pair[0]].append(rounded_correlation)
+                group_correlations[pair[1]].append(rounded_correlation)
+                groups[rounded_correlation].append({pair: round(correlation, 2)})
+                print pair, round(correlation, 2), rounded_correlation
+
+            # print date, market_correlations, group_correlations
+            for k in market_correlations.keys():
+                print k, market_correlations[k], reduce(mul, market_correlations[k]), log(reduce(mul, market_correlations[k]))
+
+            # TODO if there is only one group, no need to calculate average AND not need to use group at first place
+            if len(group_correlations) <= 1:
+                for k in group_correlations.keys():
+                    print k, group_correlations[k], sum(group_correlations[k]) / len(group_correlations[k]), log(sum(group_correlations[k]) / len(group_correlations[k]))
+
+    def __volatility_scalars(self, date):
+        markets = [p.market() for p in self.__positions]
+        if len(markets) >= 2:
+            market_ids = {str(m.id()): m for m in markets}
+
+    def __diversification_multiplier(self, date):
+        return 1.0
