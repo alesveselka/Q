ff0eb2442fbd132a325566f168fda740022399cd
alesveselka <ales.veselka@gmail.com>
2017-09-08 11:03:28 +0200

    Correct warning message

--- main/system/main.py
+++ main/system/main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     if len(sys.argv) == 2 and len(sys.argv[1]):
         Initialize(sys.argv[1])
     else:
-        print 'Expected one argument - name of the investment universe'
+        print 'Expected one argument - name of the simulation'
