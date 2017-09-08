657a715ae839da7a177c9752276c22e7e2145298
alesveselka <ales.veselka@gmail.com>
2017-09-08 19:51:33 +0200

    Refactor volatility scalar calculations

--- main/system/portfolio.py
+++ main/system/portfolio.py
@@ -39,21 +39,23 @@ class Portfolio(object):
         equity = float(self.__account.equity(date))
         cash_volatility_target = equity * self.__volatility_target
         daily_cash_volatility_target = cash_volatility_target / 16
-        
+
         print date, equity, cash_volatility_target, daily_cash_volatility_target
 
         markets = [p.market() for p in self.__positions]
         if len(markets):
             price_date = date
+            prices = {}
             correlation_data = {}
             for market in markets:
                 market_data, previous_data = market.data(date)
                 data = previous_data if previous_data else market.data_range(end_date=date)[-1]
                 price_date = data[Table.Market.PRICE_DATE]
+                prices[market.id()] = data[Table.Market.SETTLE_PRICE]
                 correlation_data[market.id()] = market.correlation(price_date)
 
             correlations, market_weights = self.__correlation_weights(correlation_data)
-            volatility = self.__volatility_scalars(price_date, correlation_data, daily_cash_volatility_target)
+            volatility = self.__volatility_scalars(price_date, prices, correlation_data, daily_cash_volatility_target)
 
             # volatility = {1: (0.00625, 10), 2: (0.00625, 10)}
             # correlations = {('1', '2'): -0.6}
@@ -70,6 +72,18 @@ class Portfolio(object):
                     round(volatility[market_id][1] * w * dm, 2)  # final position!
 
     def __correlation_weights(self, correlation_data):
+        """
+        Calculate weights for each market based on correlations
+        
+        1. For each market, sum correlations with every other market;
+        2. group market correlations and also sum these as 'group_weights';
+        3. final weight for each market equals 'log(market correlations ^ group correlations) / sum of all correlations'
+        
+        :param correlation_data:    dict of market ID as a key and record from 'market_correlation' as a value
+        :return:                    tuple of
+                                        dict of market correlations with market IDs as keys, and
+                                        dict of correlation weights for each market with market IDs as keys
+        """
         # TODO The 'Handcrafting' method assumes the assets have the same expected standard deviation of returns!
         market_ids = correlation_data.keys()
         correlations = {}
@@ -107,34 +121,25 @@ class Portfolio(object):
 
         return correlations, market_weights
 
-    def __volatility_scalars(self, date, correlation_data, daily_cash_volatility_target):
-        markets = [p.market() for p in self.__positions]
-        # scalars = {}
+    def __volatility_scalars(self, date, prices, correlation_data, daily_cash_volatility_target):
+        """
+        Calculate price volatility and volatility scalars for each market
+        
+        :param date:                            date on which to calculate
+        :param prices:                          dict of prices and their respective market IDs as keys
+        :param correlation_data:                dict of market ID as a key and record from 'market_correlation' as a value
+        :param daily_cash_volatility_target:    equity x volatility target / sqrt(256)
+        :return:                                dict of tuples(price volatility, volatility scalar) with market IDs as keys
+        """
         volatility = {}
-        for market in markets:
-            market_data, previous_data = market.data(date)
-            data = previous_data if previous_data else market.data_range(end_date=date)[-1]
-            price_date = data[Table.Market.PRICE_DATE]
-            price = data[Table.Market.SETTLE_PRICE]
+        for market in [p.market() for p in self.__positions]:
             point_value = market.point_value()
-            block_value = price * point_value
-            correlation_data = market.correlation(price_date)
-            price_volatility = correlation_data[Table.MarketCorrelation.VOLATILITY]
+            block_value = prices[market.id()] * point_value
+            price_volatility = correlation_data[market.id()][Table.MarketCorrelation.VOLATILITY]
             instrument_currency_volatility = price_volatility * block_value
-            instrument_value_volatility = self.__account.base_value(instrument_currency_volatility, market.currency(), price_date)
+            instrument_value_volatility = self.__account.base_value(instrument_currency_volatility, market.currency(), date)
             volatility_scalar = daily_cash_volatility_target / instrument_value_volatility
-            # scalars[market.id()] = volatility_scalar
             volatility[market.id()] = price_volatility, volatility_scalar
-            print \
-                market.code(), \
-                int(daily_cash_volatility_target), \
-                price, \
-                point_value, \
-                block_value, \
-                price_volatility, \
-                instrument_currency_volatility, \
-                instrument_value_volatility, \
-                volatility_scalar
 
         return volatility
 
