73a12135e9e19f309d61e27f59c33e1a890daa31
alesveselka <ales.veselka@gmail.com>
2017-09-15 21:17:54 +0200

    Implement 'removed_positions' function to Portfolio object

--- main/system/portfolio.py
+++ main/system/portfolio.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from collections import defaultdict
+
 
 class Portfolio(object):
 
@@ -7,13 +9,15 @@ class Portfolio(object):
         self.__account = account
         self.__positions = []
         self.__closed_positions = []
+        self.__removed_positions = defaultdict(list)
 
     def add_position(self, position):
         self.__positions.append(position)
 
-    def remove_position(self, position):
+    def remove_position(self, date, position):
         self.__closed_positions.append(position)
         self.__positions.remove(position)
+        self.__removed_positions[date].append(position)
 
     def market_position(self, market):
         positions = [p for p in self.__positions if p.market() == market]
@@ -24,3 +28,6 @@ class Portfolio(object):
 
     def closed_positions(self):
         return self.__closed_positions
+
+    def removed_positions(self, date):
+        return self.__removed_positions[date] if date in self.__removed_positions else []
