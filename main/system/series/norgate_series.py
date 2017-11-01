c5e7828921dbaa62dca1c464e98b569d69a0cb0b
alesveselka <ales.veselka@gmail.com>
2017-11-01 21:33:15 +0100

    Ïmplement missing abstract methods

--- main/system/series/norgate_series.py
+++ main/system/series/norgate_series.py
@@ -81,6 +81,30 @@ class NorgateSeries(MarketSeries):
         """
         return None
 
+    def previous_contract(self, contract):
+        """
+        Return contract code before the one passed in
+        
+        :return:    string code representing the previous contract
+        """
+        return None
+
+    def next_contract(self, contract):
+        """
+        Return contract code after the one passed in
+        
+        :return:    string code representing the next contract
+        """
+        return None
+
+    def contract_data(self, contract, date):
+        """
+        Return data of contract passed in
+        
+        :return:    tuple representing one day record
+        """
+        return None
+
     def rolls(self):
         """
         Return empty list since the continuous series doesn't have roll data
@@ -96,4 +120,4 @@ class NorgateSeries(MarketSeries):
         :param date:    date of the roll
         :return string: contract symbol
         """
-        raise None
+        return None
